d5547c38bb9061ee15143e5da69c23387a51fab2
slonektb <85453976+slonektb@users.noreply.github.com>
2023-08-16 12:21:24 +0500

    second

--- src/test/java/com/skyeng/demoSkyeng/service/MailingServiceTest.java
+++ src/test/java/com/skyeng/demoSkyeng/service/MailingServiceTest.java
@@ -1,7 +1,19 @@
 package com.skyeng.demoSkyeng.service;
 
+import com.skyeng.demoSkyeng.repository.MailingRepository;
+import com.skyeng.demoSkyeng.repository.MailingStateRepository;
+import com.skyeng.demoSkyeng.repository.PostOfficeRepository;
+import org.junit.jupiter.api.BeforeAll;
+import org.mockito.Mockito;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 public class MailingServiceTest {
+
+    @BeforeAll
+    public void setUp () {
+        MailingRepository mailingRepositoryMock = Mockito.mock(MailingRepository.class);
+        MailingStateRepository mailingStateRepositoryMock = Mockito.mock(MailingStateRepository.class);
+        PostOfficeRepository postOfficeRepositoryMock = Mockito.mock(PostOfficeRepository.class);
+    }
 }
--- src/test/java/com/skyeng/demoSkyeng/service/PostOfficeServiceTest.java
+++ src/test/java/com/skyeng/demoSkyeng/service/PostOfficeServiceTest.java
@@ -1,7 +1,18 @@
 package com.skyeng.demoSkyeng.service;
 
+import com.skyeng.demoSkyeng.repository.MailingRepository;
+import com.skyeng.demoSkyeng.repository.MailingStateRepository;
+import com.skyeng.demoSkyeng.repository.PostOfficeRepository;
+import org.junit.jupiter.api.BeforeAll;
+import org.mockito.Mockito;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 public class PostOfficeServiceTest {
+
+    @BeforeAll
+    public void setUp () {
+        PostOfficeRepository postOfficeRepositoryMock = Mockito.mock(PostOfficeRepository.class);
+    }
+
 }
